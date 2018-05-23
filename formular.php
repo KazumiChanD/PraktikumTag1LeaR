d275eec5f4f731284373b955847b6699fafe76da
Lea Reimann <LeaReimann1999@gmail.com>
2018-05-23 17:09:27 +0200

    Aufgabe8 Formular vervollständigt

--- formular.php
+++ formular.php
@@ -1,3 +1,8 @@
 <?php
-echo "Hello World";
+$anrede = $_POST["anrede"];
+$email = $_POST ["email"];
+$name = $_POST ["name"];
+$betreff = $_POST ["betreff"];
+$nachricht = $_POST ["nachricht"];
+echo "Anrede: $anrede Email: $email Name: $name Betreff: $betreff Nachricht: $nachricht";
 ?>
